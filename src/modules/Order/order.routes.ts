f551576d7239d2ac4e8e2bab2ac9aa919a32ad55
unknown <raihanshorker56@gmail.com>
2025-04-25 17:15:08 +0600

    update api

--- src/modules/Order/order.routes.ts
+++ src/modules/Order/order.routes.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 // router.post('/', OrderController.createOrder);
 router.post('/order',auth(USER_ROLE.customer), OrderController.createOrder)
 //   router.get(auth(UserRole.user), orderController.getOrders);
-router.get('/orders', auth(USER_ROLE.mealProvider), OrderController.getAllOrders);
-router.put('/profile/:userId', auth(USER_ROLE.customer),  validateRequest(AuthValidation.updateUserValidationSchema), OrderController.updateProfile);
+router.get('/orders', auth(USER_ROLE.customer), OrderController.getAllOrders);
+router.put('/profile/:userId',validateRequest(AuthValidation.updateUserValidationSchema), OrderController.updateProfile);
 router.get("/:id", auth(USER_ROLE.customer), OrderController.getSingleOrders);
 export const OrderRoute = router;
